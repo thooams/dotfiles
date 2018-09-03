923787165ff4dff21ec5022ee0b16e98a82f00e8
Thomas HUMMEL <thomas@hummel.link>
2018-09-03 16:32:09 +0200

    add i3 config

---
+++ config/i3/bing-wallpaper.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+imagepath="$HOME/Pictures/bing-wallpaper.jpg"
+urlpath=$( \
+curl "https://www.bing.com/HPImageArchive.aspx?format=rss&idx=0&n=1&mkt=en-US" \
+| xmllint --xpath "/rss/channel/item/link/text()" - \
+| sed 's/1366x768/1920x1080/g' \
+)
+curl "https://www.bing.com$urlpath" -o $imagepath
+feh --bg-fill $imagepath
